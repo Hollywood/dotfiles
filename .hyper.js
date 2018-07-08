11918944f8ab4187a499b43d33fff32c3c908e9f
Stefan Stölzle <stoe@github.com>
2018-07-08 19:20:58 +0200

    Rework all the things™

---
+++ .hyper.js
@@ -0,0 +1,28 @@
+module.exports = {
+  config: {
+    shell: '/bin/zsh',
+    fontSize: 16,
+    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    cursorShape: 'BLOCK',
+    cursorBlink: true,
+    colors: {},
+    windowSize: [1706, 1062],
+    hyperlinks: {
+      clickAction: 'open',
+      defaultBrowser: true,
+    },
+    materialBox: {
+      scheme: 'snazzy',
+      autoHideTitle: true,
+    },
+    hyperStatusLine: {
+      footerTransparent: true,
+    },
+    hyperTabs: {
+      trafficButtons: true,
+      tabIconsColored: true,
+    },
+  },
+  plugins: ['hyperlinks', 'hyper-pane', 'hypercwd', 'hyper-material-box', 'hyper-tabs-enhanced'],
+  localPlugins: [],
+};
